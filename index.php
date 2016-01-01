9d7d5ce400e571db3b294cdef4207533a01b36e8
Clément Hampaï <cypress@cypressxt.net>
2016-01-01 19:39:03 +0100

    update markdown style

--- index.php
+++ index.php
@@ -4,8 +4,16 @@
         <meta charset="utf-8" />
         <link rel="stylesheet" href="markdown.css">
         <title>Dev @ GES</title>
+        <style>
+            body{
+                max-width: 1000px;
+                margin-left: auto;
+                margin-right: auto;
+            }
+        </style>
     </head>
     <body>
+        <article class="markdown-body">
 <?php
     $url = 'https://github.com/CypressXt/IT_Geneva-e-sport/blob/master/readme.md';
     $content = file_get_contents($url);
@@ -13,5 +21,6 @@
     $second_step = explode("</div>", $first_step[1]);
     echo $second_step[0];
  ?>
+        </article>
     </body>
 </html>
